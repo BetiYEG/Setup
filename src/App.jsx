74b94f2b21d576450cb65ed03bc75bd703a51f98
BetiYEG <betelhemyegzaw468@gmail.com>
2024-04-10 20:08:24 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { useEffect } from "react";
 import HomePage from "./components/common/Button";
-import { Button } from "./components/common/Button/button";
 import axios from "axios";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 function App() {
   useEffect(() => {
     axios.get(" https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8")
@@ -11,10 +13,13 @@ function App() {
   }, []);
   return (
     <>
-      <div className="px-40">
-        <HomePage />
-        <Button>Hello</Button>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/HomePage" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
